3b24fa38ad747826b3327fc8924d813c6b8a3b61
Besteven <stefanostei18@gmail.com>
2022-12-02 23:42:40 +0200

    initial commit

---
+++ pinakes.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+#define D_max 50
+
+int main(){
+    int B[3][11];
+    int A[D_max][D_max];
+
+    for(int i = 0; i < D_max; i++){
+        for(int j = 0; j < D_max; j++){
+            A[i][j] = 0;
+        }
+    }
+
+}
